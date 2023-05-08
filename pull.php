a9bec26480b1720db12561b8a3b54a4c1056f050
kuba <Jakub Troczynski>
2023-05-08 19:23:31 +0200

    commit

--- pull.php
+++ pull.php
@@ -8,7 +8,7 @@ if ( $_POST['payload'] ) {
 putenv('PATH=/usr/local/bin');
 echo shell_exec('cd /var/www/u202319/data/www/woo.dmn.gg && /usr/bin/git pull origin main 2>&1');
 echo shell_exec('/usr/bin/whoami 2>&1');
-echo 'mateusz52';
+echo 'mateusz52h';
 }
 //boftff
 ?>
